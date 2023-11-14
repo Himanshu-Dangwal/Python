03f6a9ca309d30de5534eec04e6c7820fcbd3aa7
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2023-11-14 20:35:06 +0530

    Input and some basic comcepts

---
+++ concepts.py
@@ -0,0 +1,18 @@
+# Lists - > Ordered Collection of items. It is mutable
+
+list1 = [1,2,3,[4,5],["apple"],"banana",3.24]
+print(list1)
+
+# Tuple - > Ordered Collection of items. It is immutable.
+
+tuple1 = (1,2,3,("Himanshu"))
+print(tuple1)
+
+tuple1 = (5,6,7)    #Will check this again in tuples
+print(tuple1)
+
+
+# Dictionary
+
+dict1 = {"name":"Himanshu","age":23}
+print(dict1)
