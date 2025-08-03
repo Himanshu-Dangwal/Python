74859db33af84dc3fe984ec19ca2540aeb38af31
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2025-08-02 20:24:03 -0700

    Added a new app

--- Django/myproject/myproject/urls.py
+++ Django/myproject/myproject/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",views.homePage),
     path("about/",views.about),
+    path("posts/",include("posts.urls")),
 ]
