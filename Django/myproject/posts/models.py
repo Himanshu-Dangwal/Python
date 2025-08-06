928f5f63c0f72b6c6423d0922bea7d26eabf5549
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2025-08-06 16:40:26 -0700

    Django Admin superuser and url management

--- Django/myproject/posts/models.py
+++ Django/myproject/posts/models.py
@@ -1,8 +1,26 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=50)
     body = models.TextField()
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.title)
+            slug = base_slug
+            counter = 1
+
+            # Keep generating a unique slug until it's not taken
+            while Post.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"The title is {self.title}"
