3275a0425175caefa6c40b0ab54963da49c6ef46
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2025-08-14 10:57:49 -0700

    a

--- Django/myproject/notes/models.py
+++ Django/myproject/notes/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Notes(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=40)
     description = models.TextField()
     tags = models.CharField(max_length=60)
